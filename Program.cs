5ee14761e1115cf0b65578da83aacd045dd7ac04
kanakbabar <kanakbabar999@gamil.com>
2021-10-28 09:50:16 +0530

    [kanak] Add : Creat a empty board

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+     class Tic_Tac_Toe
+    {
+     
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Wel-Come To Tic Tac Toe game :");
+            //TicTacToe.CreatBoard();
+            //ChoosingMark.choosingXorO();
+             Board.ShowBoard();
+            Console.ReadLine();
+        }
+      
+     }
+ }
---
+++ TicTacToe.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+   public class TicTacToe
+    {
+        public static char[] element;
+
+        public static void CreatBoard()
+        {
+            element = new char[10];
+            for (int i = 1; i < 10; i++)
+            {
+                element[i] = ' ';
+            }
+            Console.WriteLine("Empty board are created .");
+        }
+    }
+}
