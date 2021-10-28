fb6ccf018571a26e25a6ad93fdb28ee1f51eb643
kanakbabar <kanakbabar999@gamil.com>
2021-10-28 20:00:56 +0530

    [kanak] Add : creat a board

---
+++ Board.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class Board
+    {
+        public static object element;
+
+        public static void ShowBoard()
+        {
+            element = new char[10];
+            Console.WriteLine("\n  " + element + " | " + element + " | " + element + " ");
+            Console.WriteLine(" ----------- ");
+            Console.WriteLine("  " + element + " | " + element + " | " + element + " ");
+            Console.WriteLine(" ----------- ");
+            Console.WriteLine("  " + element + " | " + element + " | " + element + " \n");
+
+        }
+    }
+}
